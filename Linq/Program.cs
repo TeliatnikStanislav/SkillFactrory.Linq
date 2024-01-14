542a7de14481a74f2bf468833639358b188c3c6d
Stanislav <slava.telyatnik@mail.ru>
2024-01-14 13:40:37 +0100

    Добавьте файлы проекта.

---
+++ Linq/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>()
+{
+   1,
+   "Сергей ",
+   "Андрей ",
+   300,
+};
+            var selectedobjects = (from p in objects where p is string orderby p select p);
+            foreach (var o in selectedobjects)
+            {
+                Console.WriteLine(o.ToString());
+            }
+
+            foreach (var num in objects.Where(num => num is string).OrderBy(num => num))
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
