bead116a0e86d22dadbe61191fdebcc7540b76e0
Stanislav <slava.telyatnik@mail.ru>
2024-01-14 14:06:46 +0100

    14.1.1

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -27,7 +27,31 @@ namespace Linq
             {
                 Console.WriteLine(num);
             }
+
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
+
+            foreach (var city in russianCities.Where(c => c.Name.Length <= 10).OrderBy(c => c.Name.Length))
+            {
+                Console.WriteLine($"{city.Name}");
+            }
             Console.ReadKey();
+        }
+
+        public class City
+        {
+            public City(string name, long population)
+            {
+                Name = name;
+                Population = population;
+            }
+
+            public string Name { get; set; }
+            public long Population { get; set; }
         }
     }
 }
